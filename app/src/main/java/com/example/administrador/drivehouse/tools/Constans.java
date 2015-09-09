147ec7644eb6e1d492ff53981f50022f26c2da8b
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-09-09 14:54:25 -0500

    constats

---
+++ app/src/main/java/com/example/administrador/drivehouse/tools/Constans.java
@@ -0,0 +1,19 @@
+package com.example.administrador.drivehouse.tools;
+
+/**
+ * Created by Jhonatan Quilca
+ */
+public class Constans {
+    /**
+     * Puerto que utilizas para la conexión.
+     * Dejalo en blanco si no has configurado esta carácteristica.
+     */
+    private static final String PUERTO_HOST = "80";
+
+    /**
+     * Dirección IP de genymotion o AVD
+     */
+    private static final String IP = "http://192.168.1.2:";
+
+
+}
