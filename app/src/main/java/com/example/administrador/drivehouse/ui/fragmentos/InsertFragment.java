f4eab95f9a2d5d9078f516ab52eabed169d19a86
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-09-22 16:11:17 -0500

    visualizacion del view

---
+++ app/src/main/java/com/example/administrador/drivehouse/ui/fragmentos/InsertFragment.java
@@ -0,0 +1,31 @@
+package com.example.administrador.drivehouse.ui.fragmentos;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrador.drivehouse.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InsertFragment extends Fragment {
+
+
+    public InsertFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert, container, false);
+    }
+
+
+}
