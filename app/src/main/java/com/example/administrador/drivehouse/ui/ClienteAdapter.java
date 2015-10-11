d7e64996aa433c237841e592db791b91e5448d9b
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-10-11 11:05:08 -0500

    elimine comrntario

--- app/src/main/java/com/example/administrador/drivehouse/ui/ClienteAdapter.java
+++ app/src/main/java/com/example/administrador/drivehouse/ui/ClienteAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.administrador.drivehouse.R;
 import com.example.administrador.drivehouse.models.Cliente;
